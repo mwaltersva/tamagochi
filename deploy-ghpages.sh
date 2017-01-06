80557e109bfa5138e8503d54f8e39ea210b22b04
MatthewWalters <mawalters@beambros.com>
2017-01-06 16:46:11 -0500

    I think it worked...

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -8,5 +8,5 @@ git config user.name "MW (Travis)"
 git config user.email "mwalters1984@gmail.com"
 git add .
 git commit -m "deployed to ghpages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
 )
