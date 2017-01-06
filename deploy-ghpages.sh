0f77e5d13e77ffae396dd18f8365154a8b604fdf
MatthewWalters <mawalters@beambros.com>
2017-01-06 15:54:44 -0500

    lets test ghpages

---
+++ deploy-ghpages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf dist || exit 0;
+gulp build
+( cd dist
+git init
+git config user.name "MW (Travis)"
+git config user.email "mwalters1984@gmail.com"
+git add .
+git commit -m "deployed to ghpages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
